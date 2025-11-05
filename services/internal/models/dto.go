463e0cc23d5b0b4e97e8c6c9c89bca36eed9a684
Ghozi-Waridi <ghoziwaridi@gmail.com>
2025-11-06 04:14:11 +0700

    :star: ADD: models

---
+++ services/internal/models/dto.go
@@ -0,0 +1,18 @@
+package models
+
+type RegisterInput struct {
+	Name        string `jaon:"name" binding:"required"`
+	Email       string `json:"email" binding:"required, email"`
+	Password    string `json:"password" binding:"required, min=6"`
+	CompanyName string `json:"company_name" binding:"required"`
+}
+
+type LoginInput struct {
+	Email    string `json:"email" binding:"required, email"`
+	Password string `json:"password" binding:"required, min=6"`
+}
+
+type LoginResponse struct {
+	Token string `json:"token"`
+	User  User   `json:"user"`
+}
